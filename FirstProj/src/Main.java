585f0eaa0d506f5d92d7851d56b86948a9560e13
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:12:51 +0900

    FirstProj push

---
+++ FirstProj/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Yang Gyu Hee");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FirstProj/src/Number.java
@@ -0,0 +1,10 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = -100;
+		System.out.println(n1);
+	}
+
+}
