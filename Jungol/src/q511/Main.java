802f694fc4cadbbb8d858e89a121b4f0adeaef14
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:22:01 +0900

    first day last thing

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,13 @@
+package q511;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n1 = 55;
+        int n2 = 2008;
+        System.out.println(n1 + " - " + "10" + " = " + (n1-10));
+        System.out.println(n2 + " - " + "1999" + " = " + (n2-1999));
+    }
+}
+
